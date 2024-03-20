71855561e94ab4824a769d072f858274cd445357
Alma123A <alma.tisch1@gmail.com>
2024-03-20 17:06:56 +0200

    fix errores in fire base

--- app/src/main/java/com/example/memorygame/LoginActivity.java
+++ app/src/main/java/com/example/memorygame/LoginActivity.java
@@ -1,32 +1,31 @@
 package com.example.memorygame;
 
-import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.WindowDecorActionBar;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.memorygame.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
@@ -36,12 +35,11 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Objects;
 
-public class MainActivity2 extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     FirebaseAuth auth;
     GoogleSignInClient googleSignInClient;
     ShapeableImageView imageView;
     TextView name, mail;
-
     private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
         public void onActivityResult(ActivityResult result) {
@@ -55,12 +53,12 @@ public class MainActivity2 extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 auth = FirebaseAuth.getInstance();
-                                Glide.with(MainActivity2.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
+                                Glide.with(LoginActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
                                 name.setText(auth.getCurrentUser().getDisplayName());
                                 mail.setText(auth.getCurrentUser().getEmail());
-                                Toast.makeText(MainActivity2.this, "Sign in successfully!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LoginActivity.this, "Signed in successfully!", Toast.LENGTH_SHORT).show();
                             } else {
-                                Toast.makeText(MainActivity2.this, "Failed to sign in: " + task.getException(), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(LoginActivity.this, "Failed to sign in: " + task.getException(), Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
@@ -70,11 +68,11 @@ public class MainActivity2 extends AppCompatActivity {
             }
         }
     });
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
         FirebaseApp.initializeApp(this);
         imageView = findViewById(R.id.profileImage);
@@ -82,9 +80,48 @@ public class MainActivity2 extends AppCompatActivity {
         mail = findViewById(R.id.mailTV);
 
         GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.id.string.client_id))
+                .requestIdToken(getString(R.string.client_id))
                 .requestEmail()
                 .build();
-                googleSignInClient = GoogleSignIn.getClient(MainActivity2.this,options);
+        googleSignInClient = GoogleSignIn.getClient(LoginActivity.this, options);
+
+        auth = FirebaseAuth.getInstance();
+
+        SignInButton signInButton = findViewById(R.id.signIn);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = googleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(intent);
+            }
+        });
+
+        MaterialButton signOut = findViewById(R.id.signout);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+                    @Override
+                    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                        if (firebaseAuth.getCurrentUser() == null) {
+                            googleSignInClient.signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(LoginActivity.this, "Signed out successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+                                }
+                            });
+                        }
+                    }
+                });
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
+
+        if (auth.getCurrentUser() != null) {
+            Glide.with(LoginActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
+            name.setText(auth.getCurrentUser().getDisplayName());
+            mail.setText(auth.getCurrentUser().getEmail());
+        }
     }
 }
